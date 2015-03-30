2a26be59fb5db70f3652f5638f891ff2258e0d8f
Neil Atkinson <neil@new-bamboo.co.uk>
2015-03-30 19:02:27 +0100

    QuickDatFormatter can parse ‘yyyy-mm-dd’ which is the format produced by Date.new.to_s

--- spec/formatters/quick_date_formatter_spec.rb
+++ spec/formatters/quick_date_formatter_spec.rb
@@ -7,7 +7,15 @@ describe QuickDateFormatter do
     end
 
     it 'correctly parses dd/mm/yy' do
-      expect(QuickDateFormatter.parse('11/1/11')).to eq(Date.new(2011, 1, 11))
+      expect(QuickDateFormatter.parse('11/1/12')).to  eq(Date.new(2012, 1, 11))
+    end
+
+    it 'correctly parses yyyy-mm-dd' do
+      expect(QuickDateFormatter.parse('2011-1-12')).to eq(Date.new(2011, 1, 12))
+    end
+
+    it 'correctly parses yy-mm-dd' do
+      expect(QuickDateFormatter.parse('11-1-12')).to eq(Date.new(2011, 1, 12))
     end
 
     it 'allows a date from the 90s' do
