701229dad6f90c908cbabb3758af88617ae433ea
Ben Pickles <spideryoung@gmail.com>
2015-04-02 21:58:59 +0100

    Don't worry about `SafeBuffer`s just use `#concat`.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,11 @@ module ApplicationHelper
     items.unshift(link_to('Home', root_path))
     divider = content_tag(:span, '/', class: 'divider')
 
-    list_items = items.inject(ActiveSupport::SafeBuffer.new) do |output, item|
-      output += content_tag(:li, h(item) + divider)
+    content_tag :ul, class: 'breadcrumb' do
+      items.each do |item|
+        concat content_tag(:li, h(item) + divider)
+      end
     end
-
-    content_tag :ul, list_items, class: 'breadcrumb'
   end
 
   def breadcrumbs_for_loan(loan, *extras)
