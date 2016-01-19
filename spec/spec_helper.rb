d831e7757a8bdae280188ba679e870a089c5c37c
Ben Pickles <spideryoung@gmail.com>
2016-01-19 17:02:56 +0000

    Let me easily re-run all the spec failures.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,11 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = false
   end
 
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
@@ -66,11 +71,6 @@ RSpec.configure do |config|
   # get run.
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
-
-  # Allows RSpec to persist some state between runs in order to support
-  # the `--only-failures` and `--next-failure` CLI options. We recommend
-  # you configure your source control system to ignore this file.
-  config.example_status_persistence_file_path = "spec/examples.txt"
 
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
