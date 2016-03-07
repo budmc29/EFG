8d124c37832baa9a83e8bdcff53d44fff8621374
Nick Charlton <nick@nickcharlton.net>
2016-03-07 11:43:26 +0000

    Fix where the lender order is non-determinate.

--- spec/requests/reports/realisation_report_spec.rb
+++ spec/requests/reports/realisation_report_spec.rb
@@ -1,11 +1,36 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe 'Realisation report' do
-  let(:loan1) { FactoryGirl.create(:loan, :settled, settled_on: 10.days.ago.to_date )}
-  let(:loan2) { FactoryGirl.create(:loan, :settled, settled_on: 11.days.ago.to_date )}
-  let(:loan3) { FactoryGirl.create(:loan, :settled, settled_on: 12.days.ago.to_date )}
-  let(:loan4) { FactoryGirl.create(:loan, :settled, settled_on: 13.days.ago.to_date )}
-  let(:loan5) { FactoryGirl.create(:loan, :settled, settled_on: 14.days.ago.to_date )}
+  let(:lender1) { FactoryGirl.create(:lender, name: "Bank 1") }
+  let(:lender2) { FactoryGirl.create(:lender, name: "Bank 3") }
+  let(:lender3) { FactoryGirl.create(:lender, name: "Bank 2") }
+  let(:lender4) { FactoryGirl.create(:lender, name: "Bank 4") }
+  let(:lender5) { FactoryGirl.create(:lender, name: "Bank 5") }
+
+  let(:loan1) do
+    FactoryGirl.create(:loan, :settled, settled_on: 10.days.ago.to_date,
+                                        lender: lender1)
+  end
+
+  let(:loan2) do
+    FactoryGirl.create(:loan, :settled, settled_on: 11.days.ago.to_date,
+                                        lender: lender2)
+  end
+
+  let(:loan3) do
+    FactoryGirl.create(:loan, :settled, settled_on: 12.days.ago.to_date,
+                                        lender: lender3)
+  end
+
+  let(:loan4) do
+    FactoryGirl.create(:loan, :settled, settled_on: 13.days.ago.to_date,
+                                        lender: lender4)
+  end
+
+  let(:loan5) do
+    FactoryGirl.create(:loan, :settled, settled_on: 14.days.ago.to_date,
+                                        lender: lender5)
+  end
 
   let!(:realisation1) { FactoryGirl.create(:loan_realisation, :pre,
                                                 realised_loan: loan1,
@@ -40,12 +65,6 @@ describe 'Realisation report' do
                                                 realised_amount: Money.new(4_000_00),
                                                 realised_on: 2.days.ago.to_date) }
 
-  let(:lender1) { loan1.lender }
-  let(:lender2) { loan2.lender }
-  let(:lender3) { loan3.lender }
-  let(:lender4) { loan4.lender }
-  let(:lender5) { loan5.lender }
-
   let(:report_start_date) { 4.days.ago.to_date.to_s(:screen) }
   let(:report_end_date) { 2.day.ago.to_date.to_s(:screen) }
 
@@ -76,17 +95,17 @@ describe 'Realisation report' do
         expect(page).to have_text 'Data extract found 3 rows'
         expect(page).to have_text "Report Start Date #{report_start_date}"
         expect(page).to have_text "Report End Date #{report_end_date}"
-        expect(page).to have_text "Lenders #{lender1.name}, #{lender2.name}, #{lender3.name}, #{lender4.name}"
+        expect(page).to have_text "Lenders #{lender1.name}, #{lender3.name}, #{lender2.name}, #{lender4.name}"
         expect(page).not_to have_text lender5.name
         click_button 'Download Report'
 
-        page.response_headers['Content-Type'].should include('text/csv')
+        expect(page.response_headers['Content-Type']).to include('text/csv')
       end
     end
 
     context 'when selecting ALL lenders' do
       it 'outputs a CSV realisation report for all lenders' do
-        all_lenders_list = Lender.all.map(&:name).join(', ')
+        all_lenders_list = Lender.order_by_name.map(&:name).join(", ")
 
         visit root_path
         click_link 'Generate Realisations Report'
@@ -106,7 +125,7 @@ describe 'Realisation report' do
         expect(page).to have_text "Lenders #{all_lenders_list}"
         click_button 'Download Report'
 
-        page.response_headers['Content-Type'].should include('text/csv')
+        expect(page.response_headers['Content-Type']).to include('text/csv')
       end
     end
 
@@ -159,11 +178,11 @@ describe 'Realisation report' do
 
         click_button 'Submit'
 
-        page.should have_text 'Data extract found 2 rows'
+        expect(page).to have_text 'Data extract found 2 rows'
         expect(page).not_to have_text 'Lenders'
         click_button 'Download Report'
 
-        page.response_headers['Content-Type'].should include('text/csv')
+        expect(page.response_headers['Content-Type']).to include('text/csv')
       end
     end
 
