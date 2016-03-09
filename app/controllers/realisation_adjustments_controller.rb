900b8c77a605af279642477d9b716af16c21ce91
Niall Mullally <niall.mullally@gmail.com>
2016-03-09 15:56:29 +0000

    Loan must be recovered or realised for realisation adjustment

--- app/controllers/realisation_adjustments_controller.rb
+++ app/controllers/realisation_adjustments_controller.rb
@@ -1,13 +1,17 @@
 class RealisationAdjustmentsController < ApplicationController
   before_filter :verify_create_permission, only: [:new, :create]
   before_filter :load_loan, only: [:new, :create]
+  before_filter :verify_loan_state
+
+  rescue_from_incorrect_loan_state_error
 
   def new
     @realisation_adjustment = LoanRealisationAdjustment.new(@loan)
   end
 
   def create
-    @realisation_adjustment = LoanRealisationAdjustment.new(@loan, params[:realisation_adjustment])
+    @realisation_adjustment = LoanRealisationAdjustment.new(
+      @loan, params[:realisation_adjustment])
     @realisation_adjustment.created_by = current_user
 
     if @realisation_adjustment.save
@@ -19,11 +23,19 @@ class RealisationAdjustmentsController < ApplicationController
   end
 
   private
+
   def verify_create_permission
     enforce_create_permission(RealisationAdjustment)
   end
 
   def load_loan
     @loan = Loan.find(params[:loan_id])
+  end
+
+  def verify_loan_state
+    unless @loan.has_state?(Loan::Recovered, Loan::Realised)
+      raise IncorrectLoanStateError.new("Tried to perform realisation " \
+        "adjustment on #{@loan.reference} in state: #{@loan.state}")
+    end
   end
 end
--- spec/controllers/realisation_adjustments_controller_spec.rb
+++ spec/controllers/realisation_adjustments_controller_spec.rb
@@ -1,27 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe RealisationAdjustmentsController do
+  let(:loan) { FactoryGirl.create(:loan) }
+
   describe "GET new" do
     def dispatch
-      get :new, loan_id: 64
+      get :new, loan_id: loan.id
     end
 
-    it_behaves_like 'AuditorUser-restricted controller'
-    it_behaves_like 'CfeAdmin-restricted controller'
-    it_behaves_like 'LenderAdmin-restricted controller'
-    it_behaves_like 'LenderUser-restricted controller'
-    it_behaves_like 'PremiumCollectorUser-restricted controller'
+    it_behaves_like "AuditorUser-restricted controller"
+    it_behaves_like "CfeAdmin-restricted controller"
+    it_behaves_like "LenderAdmin-restricted controller"
+    it_behaves_like "LenderUser-restricted controller"
+    it_behaves_like "PremiumCollectorUser-restricted controller"
+    it_behaves_like "rescue_from IncorrectLoanStateError controller" do
+      let(:current_user) { FactoryGirl.create(:cfe_user) }
+    end
   end
 
   describe "POST create" do
     def dispatch
-      post :create, loan_id: 64
+      post :create, loan_id: loan.id
     end
 
-    it_behaves_like 'AuditorUser-restricted controller'
-    it_behaves_like 'CfeAdmin-restricted controller'
-    it_behaves_like 'LenderAdmin-restricted controller'
-    it_behaves_like 'LenderUser-restricted controller'
-    it_behaves_like 'PremiumCollectorUser-restricted controller'
+    it_behaves_like "AuditorUser-restricted controller"
+    it_behaves_like "CfeAdmin-restricted controller"
+    it_behaves_like "LenderAdmin-restricted controller"
+    it_behaves_like "LenderUser-restricted controller"
+    it_behaves_like "PremiumCollectorUser-restricted controller"
+    it_behaves_like "rescue_from IncorrectLoanStateError controller" do
+      let(:current_user) { FactoryGirl.create(:cfe_user) }
+    end
   end
 end
