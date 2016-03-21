b9b3d571524b1eee23ffe5f17403f50f8c8cec15
Niall Mullally <niall.mullally@gmail.com>
2016-03-21 12:51:58 +0000

    Fix data migration generator
    The generator API changed in Rails 4.

--- lib/generators/data_migration/data_migration_generator.rb
+++ lib/generators/data_migration/data_migration_generator.rb
@@ -1,12 +1,13 @@
 class DataMigrationGenerator < Rails::Generators::NamedBase
   include Rails::Generators::Migration
-  source_root File.expand_path('../templates', __FILE__)
+  source_root File.expand_path("../templates", __FILE__)
 
-  def self.next_migration_number(path)
+  def self.next_migration_number(_path)
     Time.zone.now.utc.strftime("%Y%m%d%H%M%S")
   end
 
-  def create_migration
-    migration_template "data_migration.rb", Rails.root.join("db/data_migration", file_name)
+  def create_data_migration_file
+    template = Rails.root.join("db/data_migration", "#{file_name}.rb")
+    migration_template("data_migration.rb", template)
   end
 end
