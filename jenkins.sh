c3f5c1394f2f07f3a32696f167909cac553d6e8a
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-09-15 10:03:29 +0100

    Remove default values for secrets
    We very nearly had a security incident with EFG because of the way we add
    initializers during deployment. We ended up almost using the public version
    of the `secret_key` in production.
    
    To stop this from happening, the app must read it's config from the
    environment. This may mean that you have to adjust your development
    environments to provide these variables.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment --without=development
+
+export DEVISE_SECRET_KEY=example_secret_key
+export DEVISE_PEPPER=example_pepper
+
 RAILS_ENV=test bundle exec rake db:setup
 RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test COVERAGE=1 bundle exec rake ci:setup:rspec parallel:prepare parallel:spec
